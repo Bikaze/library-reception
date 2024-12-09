dd1cbce0bbc2a3fcb63455c743949c0a297a409f
Bikaze <clmntmugisha@gmail.com>
2024-12-09 22:42:57 +0200

    Project Setup & install tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-2xl">
+      <h1>Welcome to Vite + React + Tailwind!</h1>
+    </div>
+  );
+};
+
+export default App;
